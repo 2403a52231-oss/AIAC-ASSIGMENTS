955b77e34d85e8fb192db848309ad79c791d98c3
2403a52231-oss <2403a52231@sru.edu.in>
2025-10-25 21:59:57 +0530

    Add files via upload

---
+++ Lab-7/Task3(3).py
@@ -0,0 +1,8 @@
+with open("input.txt", "r") as data:
+    lines = data.readlines()
+with open("output.txt", "w") as output:
+    for line in lines:
+        output.write(line.upper())
+print("Processing done")
+
+
---
+++ Lab-7/Task3(4).py
@@ -0,0 +1,15 @@
+f = open("numbers.txt","r")
+nums = f.readlines()
+squares = []
+for n in nums:
+    n = n.strip()
+    if n.isdigit():
+        squares.append(int(n)*int(n))
+f2 = open("squares.txt","w")
+for sq in squares:
+    f2.write(str(sq)+"\n")
+print("squares written")    
+
+
+
+         
